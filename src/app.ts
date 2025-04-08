4f7ad4986f6de4ab0d77535ba2bba1972cd2091b
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-08 20:54:16 +0600

    create admin test with post man

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ const app:Application = express();
 
 app.use(cors())
 
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
 app.get('/',(req:Request,res:Response)=>{
     res.send({
         message:"PH health care server"
--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { useServices } from "./user.service";
 
 const createAdmin = async (req:Request,res:Response) => {
+    console.log(req.body);
    const result = await useServices.createAdminInToDB();
    res.send(result)
 }
