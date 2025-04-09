ea7ad31f0c86e7ea15377b9ca256366a93691c7c
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-09 19:02:53 +0600

    add: functional response

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -4,7 +4,12 @@ import { useServices } from "./user.service";
 const createAdmin = async (req:Request,res:Response) => {
 
    const result = await useServices.createAdminInToDB(req.body);
-   res.send(result)
+//    res.send(result)
+   res.status(200).json({
+    success:true,
+    message:'Admin Created Successfully',
+    data:result
+   })
 }
 
 
