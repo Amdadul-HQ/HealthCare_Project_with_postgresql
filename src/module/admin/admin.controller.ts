fb26fb5323ed88fede1ee7ee119e4a462e70ff67
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-09 21:21:59 +0600

    add: pick function

--- src/module/admin/admin.controller.ts
+++ src/module/admin/admin.controller.ts
@@ -1,15 +1,8 @@
 import { Request, Response } from "express";
 import { AdminServices } from "./admin.service"
+import pick from "../../app/shared/pick";
 
-const pick = (obj,keys) => {
-    const finalObject = {}
-    for(const key of keys){
-        if(obj && Object.hasOwnProperty.call(obj,key)){
-            finalObject[key] = obj[key]
-        }
-    }
-    return finalObject
-}
+
 
 const getAdmins = async(req:Request,res:Response) => {
 
