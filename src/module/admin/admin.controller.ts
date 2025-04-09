cdc8c349c9ef2b3948f2076d60811cf196ecce8d
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-09 21:17:18 +0600

    validate search fields

--- src/module/admin/admin.controller.ts
+++ src/module/admin/admin.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
 import { AdminServices } from "./admin.service"
 
+const pick = (obj,keys) => {
+    const finalObject = {}
+    for(const key of keys){
+        if(obj && Object.hasOwnProperty.call(obj,key)){
+            finalObject[key] = obj[key]
+        }
+    }
+    return finalObject
+}
 
 const getAdmins = async(req:Request,res:Response) => {
 
     try{
-        const result = await AdminServices.getAdminsFromDB(req.query)
+
+        const filter = pick(req.query,['name','email','searchTerm','contactNumber'])
+
+        const result = await AdminServices.getAdminsFromDB(filter)
             res.status(200).json({
                 success:true,
                 message:'Admin Data Fetched Successfully',
