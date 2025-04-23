c97f6bb1b8c77bb3d516bcb891ae436164c97614
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-23 19:29:06 +0600

    auth middle ware

--- src/module/user/user.routes.ts
+++ src/module/user/user.routes.ts
@@ -1,10 +1,29 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
+import { Jwthelper } from "../../app/helper/jwtHelper";
+import config from "../../app/config";
 
 
 const auth = (...roles:string[])=>{
     return async(req:Request,res:Response,next:NextFunction) => {
+        try{
+            const token = req.headers.authorization;
 
+            if(!token){
+                throw new Error("You are not authorize!!")
+            }
+
+            const varifiedUser = Jwthelper.verifyToken(token as  string,config.jwt.jwt_scret as string)
+            
+            if(roles.length && !(roles.includes(varifiedUser.role))){
+                throw new Error("You are not authorize!!")
+            }
+
+            next()
+        }
+        catch(error){
+            next(error)
+        }
     }
 }
 
