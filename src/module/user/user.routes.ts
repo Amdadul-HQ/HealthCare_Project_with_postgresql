1c0f8b2b9bfa69e41e4e79dcd5aa63fae6e12830
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-27 09:25:37 +0600

    add;file upload operation

--- src/module/user/user.routes.ts
+++ src/module/user/user.routes.ts
@@ -2,10 +2,27 @@ import express from "express";
 import { userController } from "./user.controller";
 import auth from "../../app/middleWares/auth";
 import { UserRole } from "@prisma/client";
-
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
-router.post("/",auth(UserRole.SUPER_ADMIN,UserRole.ADMIN),userController.createAdmin)
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(),'/uploads'))
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
+router.post("/",
+    auth(UserRole.SUPER_ADMIN,UserRole.ADMIN),
+    upload.single('file'), userController.createAdmin)
 
 export const UserRoutes = router;
